04562a66ab86bc9142e8f9a420a17691884f0728
Nicholas Gaydos <ngaydos@gmail.com>
2018-01-23 15:45:27 -0800

    completed transformer class

--- model.py
+++ model.py
@@ -8,17 +8,31 @@ class Transformer():
     def __init__(self):
         pass
 
-    def transform_train(df):
+    def transform_train(df, txt_columns, target_column):
         '''Takes a dataframe of the training data and returns 
         a cleaned dataset for training or testing
-        input = pandas_df including porting y-value
+        input = pandas_df including porting y-value, list of strings, string
         output = 2 pandas datasets, one containing y_value, 
         one containing all relevant x data.
         '''
-        non_zeroes = list(df.count() != 0)
-        non_zero_columns = df.columns[non_zeroes]
-        dropped_df = df[non_zero_columns]
-        X = dropped_df.drop('Assigned Group')
-        #need to add additional cleaning code here
+        bool_check = False
+        no_nas = df[df[target_column].notna()]
+        for i in range(len(txt_columns)):
+            if bool_check:
+                X += ' ' + no_nas[txt_columns[i]]
+            else:
+                X = no_nas[txt_columns[i]]
+                boolcheck = True
+        X = X.fillna('empty')
+        y = no_nas[target_column]
+        return X, y
 
-        y = dropped_df['Assigned Group'] == "Numbering"
+    def transform_test(df, txt_columns):
+        bool_check = False
+        for i in range(len(txt_columns)):
+            if bool_check:
+                X += " " + df[txt_columns[i]]
+            else:
+                X = df[txt_columns[i]]
+                boolcheck = True
+        return X
