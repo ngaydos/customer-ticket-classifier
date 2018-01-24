52314decd039aeda0d68dfbeddaef30c7ac65b28
Nicholas Gaydos <ngaydos@gmail.com>
2018-01-24 13:30:13 -0800

    update model code to actually work

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+from sklearn.pipeline import Pipeline
+
 
 df = pd.read_csv('data/realcapstonedata.csv')
 
@@ -8,9 +10,9 @@ class Transformer():
     def __init__(self):
         pass
 
-    def transform_train(df, txt_columns, target_column):
+    def transform_train(self, df, txt_columns, target_column):
         '''Takes a dataframe of the training data and returns 
-        a cleaned dataset for training or testing
+        a cleaned dataset model training or testing
         input = pandas_df including porting y-value, list of strings, string
         output = 2 pandas datasets, one containing y_value, 
         one containing all relevant x data.
@@ -21,13 +23,15 @@ class Transformer():
             if bool_check:
                 X += ' ' + no_nas[txt_columns[i]]
             else:
-                X = no_nas[txt_columns[i]]
-                boolcheck = True
+                X = no_nas[txt_columns[i]].copy()
+                bool_check = True
         X = X.fillna('empty')
         y = no_nas[target_column]
         return X, y
 
-    def transform_test(df, txt_columns):
+    def transform_test(self, df, txt_columns):
+        '''Takes in a dataframe of actual data and returns the a transformed dataframe
+        for running in the model. '''
         bool_check = False
         for i in range(len(txt_columns)):
             if bool_check:
@@ -36,3 +40,4 @@ class Transformer():
                 X = df[txt_columns[i]]
                 boolcheck = True
         return X
+
