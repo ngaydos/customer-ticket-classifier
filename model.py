e57dee208a28979e130075e918a81795cca67e31
Nicholas Gaydos <ngaydos@gmail.com>
2018-01-25 15:43:21 -0800

    added stemmer

--- model.py
+++ model.py
@@ -8,6 +8,7 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import SGDClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
+from nltk.stem.porter import PorterStemmer
 
 #taken from common words @ http://www.textfixer.com/resources/common-english-words.txt
 stopwords_ = "a,able,about,across,after,all,almost,also,am,among,an,and,any,\
@@ -33,31 +34,39 @@ class Transformer():
         output = 2 pandas datasets, one containing y_value, 
         one containing all relevant x data.
         '''
+        stemmer = PorterStemmer()
         bool_check = False
+        #remove all values where the target value does not exist
         no_nas = df[df[target_column].notna()]
+        #loops over all the text columns specified and adds a lower
+        #case version to a pandas series
         for i in range(len(txt_columns)):
             if bool_check:
                 X += ' ' + no_nas[txt_columns[i]].str.lower()
             else:
                 X = no_nas[txt_columns[i]].copy().str.lower()
                 bool_check = True
+        #fills any empty fields with a string that says "empty"
         X = X.fillna('empty')
+        #removes punctuation and stopwords
         X_final = pd.Series()
         for text in X:
             text_clean = text
             for char in set(string.punctuation):
+                #replace each item of punctuation with nothing
                 text_clean = text_clean.replace(char, '')
             word_list = text_clean.split()
             for word in text_clean.split():
                 if word in stopwords:
                     word_list.remove(word)
-            X_final = X_final.append(pd.Series(" ".join(word_list)), ignore_index = True)
+            X_final = X_final.append(pd.Series(stemmer.stem(" ".join(word_list))), ignore_index = True)
         y = no_nas[target_column]
         return X_final, y
 
     def transform_test(self, df, txt_columns, stopwords = set(stopwords_)):
         '''Takes in a dataframe of actual data and returns the a transformed dataframe
         for running in the model. '''
+        stemmer = PorterStemmer()
         bool_check = False
         for i in range(len(txt_columns)):
             if bool_check:
@@ -75,7 +84,7 @@ class Transformer():
             for word in text_clean.split():
                 if word in stopwords:
                     word_list.remove(word)
-            X_final = X_final.append(pd.Series(" ".join(word_list)), ignore_index = True)
+            X_final = X_final.append(pd.Series(stemmer.stem(" ".join(word_list))), ignore_index = True)
         return X_final
 
 class Modeler():
