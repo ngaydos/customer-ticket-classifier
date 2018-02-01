a4728371df233fdfc6e3523f1172dbebe9ab2a58
Nicholas Gaydos <ngaydos@gmail.com>
2018-02-01 11:24:10 -0800

    grid search cleaned

--- grid_search.py
+++ grid_search.py
@@ -9,16 +9,16 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from transformer import Transformer
 
 gradient_grid = {'learning_rate': [0.005, 0.01, 0.05], 
-                          'max_depth': [5, 10, 15],
-                          'min_samples_split':[10, 40, 80, 160],
-                          'min_samples_leaf': [1, 10, 25, 50],
-                          'subsample': [0.8, 1.0],
+                          'max_depth': [2,3,5],
+                          'min_samples_split':[10, 40, 60, 80],
+                          'min_samples_leaf': [1, 10, 25],
+                          'subsample': [0.5],
                           'max_features': ['sqrt'], 
                           'n_estimators': [100],
                           'random_state': [1]}
 
 def grid_search(X_train, y_train, model=GradientBoostingClassifier(), params =gradient_grid):
-    searcher = GridSearchCV(model, params, scoring = 'roc_auc')
+    searcher = GridSearchCV(model, params, scoring = 'roc_auc', n_jobs = -1)
     searcher.fit(X_train, y_train)
     best_params = searcher.best_params_
     best_score = searcher.best_score_
